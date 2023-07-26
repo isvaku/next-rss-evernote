d3a31d42a6f63f75f170aa6d6703130bbb12ca31
Rigoberto Quezada <isvaku@hotmail.com>
2023-07-26 13:19:36 -0600

    Fix import

--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,6 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { env } from "~/env.mjs";
-import { appRouter, createTRPCContext } from "@erss/api";
+import { appRouter, createTRPCContext } from "@erss/api/client";
 
 // export API handler
 export default createNextApiHandler({
@@ -10,7 +10,7 @@ export default createNextApiHandler({
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
           );
         }
       : undefined,
